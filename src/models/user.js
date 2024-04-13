c717c4fe5a0473e0af9057c1cb190568cbb6781c
Sarah Diaz <81190976+SarahDiazZ@users.noreply.github.com>
2024-04-12 20:58:55 -0600

    Added password hashing

--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,45 @@
 var mongoose = require("mongoose");
+var crypto = require("crypto");
+const { type } = require("os");
 
 let userSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    userName: String,
-    email: String,
-    password: String
+    firstName: {
+        type : String,
+        required : true
+    },
+    lastName : {
+        type : String,
+        required : true
+    },
+
+    userName: {
+        type : String,
+        required : true
+    },
+    email: {
+        type : String,
+        required : true
+    },
+    hash : String,
+    salt : String //adds random data to password
+    // password: {
+    //     type : String,
+    //     required : true
+    // }
 });
+
+userSchema.methods.setPass = function(password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+
+    //Hash user's salt and password with 1000 iterations,
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, `sha512`).toString(`hex`);
+};
+
+//method to check if password entered was correct
+userSchema.methods.validPass = function(password) {
+    let hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, `sha512`).toString(`hex`);
+    return this.hash === hash;
+};
 
 // Provides all objects from user database
 userSchema.statics.listAllVideoGames = function() {
---
+++ src/routes/user.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+
+//Import user schema
+const user = require('../model/user');
+
+//user login api
+router.post('/login', (req, res) => {
+    //find user
+    if (user === null) {
+        return res.status(400).send({
+            message : 'User Not Found.'
+        });
+    } //End if
+
+    else {
+        if (user.validPass(req.body.password)) {
+            return res.status(201).send({
+                message : 'User Logged In.'
+            });
+        } //end inner if
+
+        else {
+            return res.status(400).send({
+                message : 'Wrong Password.'
+            });
+        } //End inner else
+    } //End else
+});
+
+//user signup api
+router.post('/register', (req, res, next) => {
+    //create empty user obj
+    let newUser = new user();
+    
+    //initialize newUser with request data
+    newUser.name = req.body.name,
+
+    newUser.email = req.body.email,
+
+    newUser.password = req.body.password
+
+    //call setPass function to hash password
+    newUser.setPass(req.body.password);
+
+    //save the new user to the DB
+    newUser.save((err, user) => {
+        if (err) {
+            return res.status(400).send({
+                message : "Failed to add new user"
+            });
+        } //end if
+
+        else {
+            return res.status(201).send({
+                message : "User added successfully"
+            });
+        } //end else
+    });
+});
+
+module.exports = router;
