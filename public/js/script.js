fa8bb0b7d860d2e0fc4722cfcfd56ea13a119213
Deric Shaffer <derkstouffers@Derics-MacBook-Air.local>
2024-04-21 22:55:23 -0600

    frontend additions, added on hover effects to buttons

---
+++ public/js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // landing page stuff
+    const login_btn = document.getElementById('login');
+    const login_form = document.getElementById('login-form');
+    const welcome_page = document.getElementById('welcome-page');
+
+    login_btn.addEventListener('click', function() {
+        welcome_page.style.display = 'none';
+        login_form.style.display = 'flex';
+    });
+});
